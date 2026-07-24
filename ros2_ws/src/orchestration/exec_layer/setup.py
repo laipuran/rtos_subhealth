bf57aa670a40f5570c55500059c0e0cde5933b7d
You-Qingyu <3498554977@qq.com>
2026-07-24 22:15:07 +0800

    feat(repair):setup

--- ros2_ws/src/orchestration/exec_layer/setup.py
+++ ros2_ws/src/orchestration/exec_layer/setup.py
@@ -5,7 +5,7 @@ package_name = "exec_layer"
 setup(
     name=package_name,
     version="0.1.0",
-    packages=find_packages(exclude=["test"]),
+    packages=["exec_layer"],
     data_files=[
         ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
         (f"share/{package_name}", ["package.xml"]),
